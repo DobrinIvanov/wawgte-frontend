66bdc732101f6d8cffd0f01321b23becea1571b7
Dobrin Ivanov <dobsc2@gmail.com>
2024-05-11 13:26:28 +0000

    learning vue;

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<!-- Header.vue -->
+<template>
+  <header class="header">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'What Are We Going To Eat ?'
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Add CSS styles here */
+.header {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
+

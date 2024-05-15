80af87bf6b6b03e9f8f06de74aed4793cbccabad
Dobrin Ivanov <dobsc2@gmail.com>
2024-05-15 20:51:31 +0000

    tailwind setup and working

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+    <!-- Footer (optional) -->
+   <template>
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+          <p>&copy; 2024 What Shall We Eat?</p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+</style>

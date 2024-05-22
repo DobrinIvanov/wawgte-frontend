1414c4213bf3c9ecb3cbe7209ff4a60ce6cd6378
Dobrin Ivanov <dobsc2@gmail.com>
2024-05-22 19:49:54 +0000

    trying to work with vue router

---
+++ src/views/main/HomePage.vue
@@ -0,0 +1,20 @@
+<!-- Main.vue -->
+
+<template>
+    <div class='mx-auto container m-6'>
+
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+
+    data() {
+      return {};
+    },
+  };
+  </script>
+<style>
+</style>
+  

062ec34cda3ebf0a163b57c11482480f79c43786
Dobrin Ivanov <dobsc2@gmail.com>
2024-06-09 11:06:26 +0000

    still trying to understand axios :(

--- src/views/recipes/RecipesList.vue
+++ src/views/recipes/RecipesList.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="bg-white-1">
+    <div class="bg-orange-1">
+        <div>
+            {{ this.recipes }}
+        </div>
     </div>
 </template>
 <script>
@@ -7,12 +10,18 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            recipes: null
+            recipes: [],
         }
     },
     // TODO !
     mounted () {
        axios
+       .get("http://backend.wawgte.com/recipes")
+       .then(response => (this.recipes = response.recipes))
+       .catch(error => {
+        console.error('There was an error fetching the recipes:', error);
+      });
+       
     }
 }
 
---
+++ src/views/recipes/SingleRecipeList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-orange-1">
+        <div>
+            {{ this.recipes }}
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            recipes: [],
+        }
+    },
+    // TODO !
+    mounted () {
+       axios
+       .get("http://backend.wawgte.com/recipes")
+       .then(response => (this.recipes = response.recipes))
+       .catch(error => {
+        console.error('There was an error fetching the recipes:', error);
+      });
+       
+    }
+}
+
+</script>

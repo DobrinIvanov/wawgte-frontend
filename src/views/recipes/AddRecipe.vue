bf3699b66e3d9c23e950687c03275df625d8aac8
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-20 04:11:31 +0000

    initliaze AddRecipe.vue; requires completion

---
+++ src/views/recipes/AddRecipe.vue
@@ -0,0 +1,32 @@
+<template>
+
+// TODO AddRecipe page
+<div>
+    <form>
+    </form>
+</div>
+
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            response: {},
+        };
+    },
+    methods: {
+        async submitForm() {
+            try {
+                // Some code for submitting form
+                axios.post("http://backend.wawgte.com/recipes").then(response => {
+                this.response = response.data;
+        })
+            } catch (error) {
+                this.errorMessage = 'Recipe add failed. Please contact webmaster.';
+                console.error('Recipe add error:', error);
+            }
+        },
+    }
+}
+</script>

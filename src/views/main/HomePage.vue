e677c63c57e2191f0c145e5e5b6d0e724c5d1db9
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-20 04:12:28 +0000

    set up VueCookies and obtaining the cookie in HomePage.vue

--- src/views/main/HomePage.vue
+++ src/views/main/HomePage.vue
@@ -14,29 +14,39 @@
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import axios from 'axios';
+import VueCookies from 'vue-cookies';
 
 
 export default {
   data() {
-    return {
-      isLoggedIn: true, 
+    return { 
+      isLoggedIn: true,
+      currentUser: {
+        email: '',
+      }
     };
   },
 
   async created() {
     await this.validateToken();
   },
-
+// REQUIRES DEBUGGING
   methods: {
-    async validateToken() {
+    async validateToken(jwtoken) {
       try {
-        const jwt = getCookie('jwtWawgte');
-        const response = await axios.post('https://backend.wawgte.com/jwt', { token: jwt }, {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-        });
+        const jwtWawgte = this.$cookies.get('jwtWawgte');
+        //DEBUGGING
+        console.log('Cookie if found: ',jwtWawgte);
+        if (!jwtWawgte) {
+          this.isLoggedIn = false;
+          //DEBUGGING
+          console.log('This means no jwtWawgte was obtained(cookie)');
+          return;
+        }
+        const response = axios.post('http://backend.wawgte.com/jwt', { token: jwtWawgte });
         const result = response.data;
+        //DEBUGGING
+        console.log('response.data: ', result)
         if (result.status === 'success') {
           this.isLoggedIn = true;
         } else {

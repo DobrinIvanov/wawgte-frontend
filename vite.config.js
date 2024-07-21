d9d257c46dfc624ac49de52c94656a5a1d207255
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-21 19:53:16 +0000

    setup nodejs to run via https and set up self-signed certificate to be used

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import fs from 'fs'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,5 +20,9 @@ export default defineConfig({
   },
   server: {
 	  host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync(path.resolve('/opt/psa/var/certificates/server.key')),
+      cert: fs.readFileSync(path.resolve('/opt/psa/var/certificates/server.crt')),
+    },
   },
 })

1f4c4ec51e22bfdb2c70dbbcd8b4104cbc5b3ce7
Dobrin Ivanov <dobsc2@gmail.com>
2024-07-27 20:48:27 +0000

    save changes on AddRecipes

--- src/views/recipes/AddRecipe.vue
+++ src/views/recipes/AddRecipe.vue
@@ -1,33 +1,49 @@
 <template>
-    <div class="p-10 m-32 w-96 rounded-md bg-black-2">
-    <h1 class="scale-90 mb-6 font-semibold">Add your recipe</h1>
-    <form class="max-w-sm mx-auto" v-on:submit.prevent="addRecipe">
-      <div class="mb-5">
-        <label for="email" class="block mb-2 text-sm font-medium ">Title</label>
-        <input type="email" id="email"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
-          rounded-lg  block w-full p-2.5"
-          placeholder="example@gmail.com" required />
+  <div class="p-10 m-32 rounded-md bg-black-2 flex flex-col">
+    <div><h1 class="scale-90 mb-6 font-semibold text-center">Add your recipe</h1></div>
+    <div class="flex flex-row">
+      <div class="w-1/2">
+        
+        <form class="max-w-sm mx-auto" v-on:submit.prevent="addRecipe">
+          <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium ">Title</label>
+            <input type="email" id="email"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm
+              rounded-lg  block w-full p-2.5"
+              placeholder="example@gmail.com" required />
+          </div>
+          <div class="mb-5">
+            <label for="description" class="block mb-2 text-sm font-medium">Description</label>
+            <input id="description"
+              class="bg-gray-50 border border-gray-300 text-gray-900
+              text-sm rounded-lg block w-full p-2.5" placeholder="Description"
+              required />
+          </div>
+          <div class="mb-5">
+            <label for="instructions" class="block mb-2 text-sm font-medium">Recipe instructions</label>
+            <input id="instructions"
+              class="bg-gray-50 border border-gray-300 text-gray-900
+              text-sm rounded-lg block w-full p-2.5 h-64"
+              required />
+          </div>
+    
+          <button type="submit"
+            class="text-black-1 bg-white-2 focus:ring-4 focus:outline-none
+            font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Add</button>
+        </form>
       </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium">Description</label>
-        <input type="password" id="password"
-          class="bg-gray-50 border border-gray-300 text-gray-900
-          text-sm rounded-lg block w-full p-2.5" placeholder="Password"
-          required />
+      <div class="w-1/2 flex flex-col items-center justify-evenly">
+        <div class="w-full h-full">
+          <img src="" class="w-32 h-32 bg-black-1"/>
+        </div>
+        <div class="w-full h-full">
+          <form action="/action_page.php">
+            <input type="file" id="myFile" name="filename">
+            <input type="submit">
+          </form>
+        </div>
       </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium">Instructions</label>
-        <input type="password" id="password"
-          class="bg-gray-50 border border-gray-300 text-gray-900
-          text-sm rounded-lg block w-full p-2.5" placeholder="Password"
-          required />
-      </div>
-
-      <button type="submit"
-        class="text-black-1 bg-white-2 focus:ring-4 focus:outline-none
-        font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Add</button>
-    </form>
+    </div>
   </div>
 
 </template>
